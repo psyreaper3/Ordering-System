ddd550b6b7df05a384c0ce73327c68f26be159b9
Jay <raven_cloudwind@yahoo.com>
2024-07-01 16:53:04 +0800

    Update config.php

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ $config = parse_ini_file('printer.ini', true);
 
 // Check if the file was successfully parsed
 if ($config === false) {
-    die('Error: Unable to parse printer.ini');
+    echo('Error: Unable to parse printer.ini');
 }
 
 // Access the data as variables
@@ -33,13 +33,12 @@ $socket = @fsockopen($printer_ip, $printer_port, $errno, $errstr, 10);
 
 // Check if the connection was successful, this is to stop the operation if printer connection isn't established
 if (!$socket) {
-    die("Error: Unable to connect to printer. Error: $errno - $errstr");
+    echo("Error: Unable to connect to printer. Error: $errno - $errstr");
 }
 
 // Close the socket connection
 fclose($socket);
 
 // Proceed with further execution since the connection was successful
-//echo "Connection to printer successful. Proceeding with further execution...";
 
 ?>
